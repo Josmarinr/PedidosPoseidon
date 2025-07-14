246859a52f54b463dd7650a0004ed41dd6afdb94
Johan Sebastian Marin Rojas <josmarinr@gmail.com>
2025-07-14 17:36:43 -0500

    Initial commit

---
+++ src/main/java/com/Josmarinr/PedidosPoseidon/PedidosPoseidonApplication.java
@@ -0,0 +1,13 @@
+package com.Josmarinr.PedidosPoseidon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosPoseidonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosPoseidonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Josmarinr/PedidosPoseidon/PedidosPoseidonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Josmarinr.PedidosPoseidon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosPoseidonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
